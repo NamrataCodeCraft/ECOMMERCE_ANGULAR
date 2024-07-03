37f2adfa0d8b47f073301e395a8f33b2a4d04c72
Namrata Jaiswal <106535364+NamrataCodeCraft@users.noreply.github.com>
2024-07-03 18:41:37 +0530

    03/07/2024

--- bigbasket_ecommerce_angular/src/app/app.routes.ts
+++ bigbasket_ecommerce_angular/src/app/app.routes.ts
@@ -1,14 +1,27 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/admin/login/login.component';
+import { LandingComponent } from './pages/website/landing/landing.component';
+import { ProductsComponent } from './pages/admin/products/products.component';
+import { LayoutComponent } from './pages/admin/layout/layout.component';
 
 export const routes: Routes = [
     {
         path: '',
         redirectTo: 'login',
-        pathMatch:'full'
-    }, 
+        pathMatch: 'full'
+    },
     {
         path: 'login',
         component: LoginComponent
+    },
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            {
+                path: 'products',
+                component: ProductsComponent
+            }
+        ]
     }
 ];
